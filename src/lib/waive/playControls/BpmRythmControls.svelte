47ac62c85c66b455a177ef4a567007fbfb326a15
Sietse de Kok <sietsedekok@gmail.com>
2023-02-12 05:50:40 +0100

    ready for first deployment

--- src/lib/waive/playControls/BpmRythmControls.svelte
+++ src/lib/waive/playControls/BpmRythmControls.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <!-- BPM -->
-<div class="flex flex-row space-x-1 items-center">
+<div class="flex flex-row space-x-1 justify-center place-items-center">
 	<span class="badge rounded-l-full bg-gray-800 h-6 w-24">{bpm} bpm</span>
 
 	<div class="bg-gray-800 rounded-r-full h-6 w-12">
