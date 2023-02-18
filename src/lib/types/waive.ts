30db0b658bf5044622a6c276dc6fccc527dde737
al165 <arran.lyon@hotmail.com>
2023-02-18 17:15:35 +0100

    added FX types

--- src/lib/types/waive.ts
+++ src/lib/types/waive.ts
@@ -3,18 +3,50 @@ export enum InstrumentType {
     SNARE,
     HIHAT,
     BASS,
-    LEAD
+    LEAD,
 };
 
 export type Instrument = {
     type: InstrumentType,
-    color: string
+    color: string,
 };
+
+export enum FXType {
+    GAIN,
+    CHANNEL,
+    METER,
+    DELAY,
+    REVERB,
+    COMPRESSOR,
+    FILTER,
+    PHASER,
+    LIMITER,
+}
+
+export type FX = {
+    type: FXType,
+    label: string,
+    bypassable: boolean,
+    parameters: FXParameter[],
+}
+
+export enum FXParameterType {
+    TOGGLE,
+    LIST,
+    VALUE,
+}
+
+export type FXParameter = {
+    name: string,
+    value: number,
+    type: FXParameterType,
+    callback: (value: number) => {},
+}
 
 export type Bar = {
     index: number,
     active: boolean,
-    instrument: Instrument
+    instrument: Instrument,
 };
 
 export type NoteEvent = {
@@ -31,18 +63,18 @@ export type SynthCallback = (event: NoteEvent, time: number) => void;
 
 export type Canvas = {
     w: number,
-    h: number
+    h: number,
 };
 
 export type Rectangle = {
     x: number, 
     y: number, 
     w: number, 
-    h: number
+    h: number,
 };
 
 export type PlayerCanvas = {
     canvas: Canvas,
     canvasColor: string;
-    fillColor: string
+    fillColor: string,
 };
