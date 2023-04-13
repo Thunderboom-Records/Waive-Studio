10cea1b1dd787e2cc7871f4790df9a1d2881c847
al165 <arran.lyon@hotmail.com>
2023-04-13 16:52:13 +0200

    changed Timer format

--- src/lib/waive/transportControls/Timer.svelte
+++ src/lib/waive/transportControls/Timer.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	import { Loop, Draw, Transport } from 'tone';
 
-	let formattedTime: string = `0:0:0`;
+	let formattedTime: string = `1 : 1 : 1`;
 
 	const transportControlsLoop = new Loop((time) => {
     	Draw.schedule(()=>{
-        	formattedTime = (Transport.position as string).slice(0, 5);
+			let digits = (Transport.position as string).split(":");
+			for(let i = 0; i < digits.length; i++){
+				digits[i] = String(Math.floor(parseInt(digits[i])) + 1);
+			}
+        	formattedTime = digits.join(' : ');
     	}, time)
 	}, "32n");
 	transportControlsLoop.start(0);
 </script>
 
-<div class="badge badge-lg text-white bg-gray-800 rounded-full py-1 w-32 text-center self-center">
+<div class="badge badge-lg text-white text-xl bg-gray-800 rounded-full py-1 w-32 text-center self-center">
 	{formattedTime}
 </div>
