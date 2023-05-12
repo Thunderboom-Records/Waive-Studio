4f23cb789b7e772bdb380a179b12c2c27d6c4644
al165 <arran.lyon@hotmail.com>
2023-05-12 14:58:05 +0200

    feat: :sparkles: scroll on BPM and Swing controls

--- src/lib/waive/transportControls/BpmRhythmControls.svelte
+++ src/lib/waive/transportControls/BpmRhythmControls.svelte
@@ -24,30 +24,39 @@
 	function updateSwing(d: number){
 		Transport.swing = Math.max(0.0, Math.min(0.5, Transport.swing + d));
 	}
+
+	function scrollBPM(event: WheelEvent){
+		event.preventDefault();
+		updateBPM(-Math.sign(event.deltaY))
+	}
+
+	function scrollSwing(event: WheelEvent){
+		event.preventDefault();
+		updateSwing(-Math.sign(event.deltaY)/100)
+	}
 </script>
 
 <!-- BPM -->
-<div class="flex flex-row space-x-1 justify-center place-items-center">
-	<span class="badge flex place-items-center justify-center rounded-l-full bg-gray-800 h-8 w-24">{bpm} bpm</span>
-	<div class="flex space-x-3 justify-center place-items-center bg-gray-800 rounded-r-full h-8 w-12">
-		<button class="btn btn-sm" on:click={() => updateBPM(-1)}> - </button>
-		<button class="btn btn-sm" on:click={() => updateBPM(1)}> + </button>
-	</div>
+<div class="flex flex-row space-x-1 justify-center place-items-center h-full">
+	<div class="badge flex place-items-center justify-center rounded-l-full bg-gray-800 h-full w-24" on:wheel={scrollBPM}>{bpm} bpm</div>
+	<button class="btn btn-sm bg-gray-800 hover:bg-gray-500 h-full w-8" on:click={() => updateBPM(-1)}> - </button>
+	<button class="btn btn-sm bg-gray-800 hover:bg-gray-500 h-full rounded-r-full w-8" on:click={() => updateBPM(1)}> + </button>
 </div>
 
 <!-- Swing Controls -->
-<div class="flex flex-row space-x-1 justify-center place-items-center">
-	<span class="badge flex place-items-center justify-center rounded-l-full  bg-gray-800 h-8 w-24">{swing}% swing</span>
-	<div class="flex space-x-3 justify-center place-items-center bg-gray-800 rounded-r-full h-8 w-12">
-		<button class="btn btn-sm" on:click={() => updateSwing(-0.01)}> - </button>
-		<button class="btn btn-sm" on:click={() => updateSwing(0.01)}> + </button>
-	</div>
+<div class="flex flex-row space-x-1 justify-center place-items-center h-full">
+	<div class="badge flex place-items-center justify-center rounded-l-full  bg-gray-800 h-full w-24" on:wheel={scrollSwing}>{swing}% swing</div>
+	<button class="btn btn-sm bg-gray-800 hover:bg-gray-500 h-full w-8" on:click={() => updateSwing(-0.01)}> - </button>
+	<button class="btn btn-sm btn btn-sm bg-gray-800 hover:bg-gray-500 h-full rounded-r-full w-8" on:click={() => updateSwing(0.01)}> + </button>
 </div>
 
 <!-- Loop Controls -->
-<div class="flex flex-row space-x-1 justify-center place-items-center">
-	<span class="badge flex place-items-center justify-center rounded-l-full  bg-gray-800 h-8 w-20">loop:</span>
-	<select bind:value={loopSelection} on:change={() => loopLength.set(loopSelection)} class="select max-w-xs rounded-r-full text-center bg-gray-800 h-8">
+<div class="flex flex-row space-x-1 justify-center place-items-center h-full">
+	<span class="badge flex place-items-center justify-center rounded-l-full  bg-gray-800 h-full w-20">loop:</span>
+	<select 
+		bind:value={loopSelection} on:change={() => loopLength.set(loopSelection)} 
+		class="select max-w-xs rounded-r-full text-center bg-gray-800 hover:bg-gray-500 h-full cursor-pointer"
+	>
 		{#each loopOptions as option, i}
 			<option value={option.value}>{option.name}</option>
 		{/each}
