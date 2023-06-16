15c1e63998dec99c4588b56acd0ede41bd157da3
Sietse de Kok <sietsedekok@gmail.com>
2023-06-16 10:47:22 +0200

    leading-9 to leading-10 everywhere

--- src/lib/waive/pattern/PatternControls.svelte
+++ src/lib/waive/pattern/PatternControls.svelte
@@ -16,13 +16,13 @@
 	</h3>
 	<button
 		on:click={() => dispatch("newClip", {variation: false})}
-		class="bg-{instrument.color}-500 hover:bg-{instrument.color}-600 btn btn-sm rounded-full w-12 h-6 font-medium text-xs capitalize leading-9"
+		class="bg-{instrument.color}-500 hover:bg-{instrument.color}-600 btn btn-sm rounded-full w-12 h-6 font-medium text-xs capitalize leading-10"
 	>
 		new
 	</button>
 	<button
 		on:click={() => dispatch("newClip", {variation: true})}
-		class="{variation ? `bg-${instrument.color}-500 hover:bg-${instrument.color}-600` : 'bg-gray-600'} font-medium leading-9 capitalize btn btn-sm rounded-full w-12 h-6 text-xs
+		class="{variation ? `bg-${instrument.color}-500 hover:bg-${instrument.color}-600` : 'bg-gray-600'} font-medium leading-10 capitalize btn btn-sm rounded-full w-12 h-6 text-xs
 		disabled:bg-gray-600/40 disabled:border-none border-none"
 		disabled={!variation}
 	>
